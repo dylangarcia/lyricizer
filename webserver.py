72a7eae334f2800d149918612050e62227579799
Dylan Garcia <dylangarcia@users.noreply.github.com>
2018-01-20 20:24:45 -0500

    Add Favicon

--- webserver.py
+++ webserver.py
@@ -2,18 +2,31 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import url_for
+from flask import redirect
+from flask import g
 from master_chain import get_master_chain
 from master_chain import create_404
+from flask_cache import Cache
+
 import markovify
 import glob
 import os
+import time
 
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 @app.before_first_request
 def before_first_request():
     create_404()
 
+@app.before_request
+def before_request():
+    g.request_start_time = time.time()
+    g.request_time = lambda: "%.5fs" % (time.time() - g.request_start_time)
+
+@cache.cached(timeout=60)
 @app.route("/", methods=["POST", "GET"])
 def index():
     path = "./Sources"
@@ -41,3 +54,7 @@ def index():
         else:
             sentences = [model.make_short_sentence(140, tries=15) for _ in range(5)]
     return render_template("index.html", sentences=sentences, start_word=start_word, sources=sources, source=source)
+
+@app.route("/favicon.ico")
+def favicon():
+    return redirect(url_for("static", filename="favicon.ico"), code=301)
