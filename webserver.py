f7912de4986bc342903ec3075f08ce2160f57168
Dylan Garcia <dylangarcia@users.noreply.github.com>
2018-01-21 19:54:32 -0500

    Attempt to SSLify the server

--- webserver.py
+++ webserver.py
@@ -8,6 +8,8 @@ from flask import g
 from master_chain import get_master_chain
 from master_chain import create_404
 from flask_cache import Cache
+from flask_sslify import SSLify
+
 
 import markovify
 import glob
@@ -15,6 +17,7 @@ import os
 
 app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+sslify = SSLify(app)
 
 @app.before_first_request
 def before_first_request():
